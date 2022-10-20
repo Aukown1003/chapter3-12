6493056cb2f49ba86b44f95a7382ba2d7ccdfd78
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-20 23:40:14 +0000

    [Start]3-8

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,14 @@
 class List < ApplicationRecord
+  #1. $ rails active_storage:install
+  #2. $ rails g scaffold user name portrait:attachment(portraitというActive Storage用属性を追加する)
+  #3.rails db:migrate
+  #4.1対１(単数枚画像投稿)で関連付けるという宣言
   has_one_attached :image
+   # 1対多(複数枚画像投稿)で関連付けるという宣言
+  # has_many_attached :portrailts
+  
+  # validates :対象のカラム名, presence(存在: true
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
