921aae6bd0a3dcbdbd2c425599b45c15051083ab
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-10-20 07:51:23 +0000

    [Start]3-4

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # postは新規投稿
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/show'
+  # lists/:id = lists/1やlists/3を
+  get 'lists/:id' => 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
